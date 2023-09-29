52ec298f6d99b810ef8694e33a4a24afc181bec8
plvinhkhangg <plvinhkhangg@gmail.com>
2023-09-29 13:24:43 +0700

    push code

---
+++ app/Http/Livewire/ChangeLanguage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class ChangeLanguage extends Component
+{
+    public function render()
+    {
+        return view('livewire.change-language');
+    }
+}
